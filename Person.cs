f359fe489bce5042e58f779baaf7bb429aa5f854
TracyRemitar <45276340+TracyRemitar@users.noreply.github.com>
2018-11-23 09:37:09 +0800

    Added Files
    Added Calculations.cs, Person.cs, Product.cs .

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    class Person
+    {
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        public string BirthDate;
+        public string Address;
+
+        public Person(string firstname, string middlename, string lastname)
+        {
+           
+        }
+
+        public string GetFullName()
+        {
+            return " ";
+        }
+        public int GetAge()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    class Product
+    {
+        public string Name;
+        public string Description;
+        public decimal Price;
+        public string Manufacturer;
+        public int Items;
+
+        public Product()
+        {
+
+        }
+        public void DeductItems(int items)
+        {
+
+        }
+
+    }
+}
